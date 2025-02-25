b37ffc1a718eeea2f666abd99c13564b4fd56457
niluse <niluse02@gmail.com>
2025-02-25 09:33:34 +0300

    feat: validation and ZOD

---
+++ src/property/dto/createProperty.dto.ts
@@ -0,0 +1,17 @@
+import { IsInt, IsPositive, IsString, Length } from "class-validator"
+import { create } from "domain"
+
+export class CreatePropertyDto{
+    @IsString({always:true})
+    // @Length(2,10,{message: "error on length. "})
+    name: string
+
+    @IsString()
+    @Length(2,10,{groups:["create"]})
+    @Length(1,15,{groups:['update']})
+    description: string
+
+    @IsInt({always:true})
+    @IsPositive()
+    area: number
+}
--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,5 +1,9 @@
-import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
-import { Http2ServerRequest } from 'http2';
+import { Body, Controller, Get, HttpCode, Param, ParseIntPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { CreatePropertyDto } from './dto/createProperty.dto';
+import { IdParamDto } from './dto/idParam.dto';
+import { ParseIdPipe } from './pipes/parseIdPipe';
+import { ZodValidationPipe } from './pipes/zodValidationPipe';
+import { createPropertySchema, CreatePropertyZodDto } from './dto/createPropertyZod.dto';
 
 @Controller('property')
 export class PropertyController {
@@ -8,19 +12,54 @@ export class PropertyController {
         return 'All Properties'
     }
 
-    // @Get(":id")
-    // findOne(@Param('id') id:string){
-    //     return `${id}`
+    @Get(":id")
+    findOne(@Param('id', ParseIntPipe) id){
+        console.log(typeof id)
+        return id
+    }
+
+    // @Get(':id/:slug')
+    // findOne(@Param('id') id, @Param("slug") slug){
+    //     return`id: ${id}\nslug:${slug}`
     // }
 
-    @Get(':id/:slug')
-    findOne(@Param('id') id, @Param("slug") slug){
-        return`id: ${id}\nslug:${slug}`
-    }
 
     @Post()
-    @HttpCode(202)
-    create(@Body() body){
+    @UsePipes(new ZodValidationPipe(createPropertySchema))
+    create(@Body() body: CreatePropertyZodDto){
         return body
     }
+
+    // @Post()
+    // @UsePipes(
+    //     new ValidationPipe({
+    //         whitelist: true, forbidNonWhitelisted: true,
+    //         groups:["create"]
+    //     }))
+    // // @HttpCode(202)
+    // create(@Body() body: CreatePropertyDto){
+    //     return body
+    // }
+    // ya da :
+    // @Post()
+    // create(@Body(new ValidationPipe({whitelist: true, forbidNonWhitelisted: true})) body: CreatePropertyDto){
+    //     return body
+    // }
+
+    @Patch(":id")
+    update(
+        // @Param() param:IdParamDto,
+        // @Param() {id}:IdParamDto,
+        @Param("id" , ParseIdPipe) id, 
+        @Body(
+        // new ValidationPipe({
+        //     whitelist: true,
+        //     forbidNonWhitelisted: true,
+        //     groups:['update'],
+        //     always:true
+        // })
+        )
+        body:CreatePropertyDto){
+            return body
+        }
 }

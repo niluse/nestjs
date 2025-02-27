a49ac933290a083a6b7183681874d3454b74ff3a
niluse <niluse02@gmail.com>
2025-02-27 11:21:15 +0300

    feat: config service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,18 +3,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import * as dotenv from 'dotenv';
+import { ConfigService } from '@nestjs/config';
 
 dotenv.config();
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private configService:ConfigService) {}
 
   @Get()
-  getHello(): string {
-    console.log("DATABASE_URL:", process.env.DATABASE_URL);
-
-    return this.appService.getHello();
+  getHello(): any {
+    return this.configService.get('dbconfig.dev.type') ? this.configService.get('dbconfig.dev.type') : 'could not get env info' ;
   }
   
   
--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,13 +1,14 @@
 import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
 import * as dotenv from 'dotenv';
 import * as path from 'path'
+import { registerAs } from "@nestjs/config";
 
 dotenv.config();
 
-export default ():PostgresConnectionOptions=>({
+export default registerAs("dbconfig.dev",():PostgresConnectionOptions=>({
     url: process.env.DATABASE_URL,
     type: "postgres",
     port: +(process.env.DATABASE_PORT!), // Doğru PostgreSQL portu
     entities: [path.resolve(__dirname,'..') + '/**/*.entity{.ts,.js}'],  //__dirname: absolute directory, current directory of the current file
     synchronize: true, // Production'da false olmalı
-})
+}))
